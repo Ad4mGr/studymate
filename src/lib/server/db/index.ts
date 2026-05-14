9e5a88cda7e380a2a1ef723687b91126cb3b1959
Adam Guerchi <adam.guerchi@esprit.tn>
2026-05-14 19:43:00 +0100

    update wrangler

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -5,6 +5,10 @@ import { env } from '$env/dynamic/private';
 
 if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
-const client = createClient({ url: env.DATABASE_URL });
+const client = createClient(
+	env.DATABASE_URL.startsWith('file:')
+		? { url: env.DATABASE_URL }
+		: { url: env.DATABASE_URL, authToken: env.TURSO_AUTH_TOKEN }
+);
 
 export const db = drizzle(client, { schema });
